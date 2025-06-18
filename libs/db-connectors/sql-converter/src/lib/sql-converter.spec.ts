a58193491cc227c854d78efd74a9995cdfa33b63
pmachaux <pierre.machaux@gmail.com>
2025-06-18 19:39:00 -0400

    Init SQL conversion

--- libs/db-connectors/sql-converter/src/lib/sql-converter.spec.ts
+++ libs/db-connectors/sql-converter/src/lib/sql-converter.spec.ts
@@ -1,7 +1,26 @@
 import { sqlConverter } from './sql-converter';
 
 describe('sqlConverter', () => {
-  it('should work', () => {
-    expect(sqlConverter()).toEqual('sql-converter');
+  it('should be able to select all when no filters are passed', () => {
+    const sql = sqlConverter('book')({}, ['id', 'title'], {});
+    expect(sql).toEqual('SELECT id, title FROM book');
+  });
+  it('should be able to add a limit', () => {
+    const sql = sqlConverter('book')({}, ['id', 'title'], {
+      limit: 10,
+    });
+    expect(sql).toEqual('SELECT id, title FROM book LIMIT 10');
+  });
+  it('should be able to add an offset', () => {
+    const sql = sqlConverter('book')({}, ['id', 'title'], {
+      offset: 10,
+    });
+    expect(sql).toEqual('SELECT id, title FROM book OFFSET 10');
+  });
+  it('should be able to add a sort', () => {
+    const sql = sqlConverter('book')({}, ['id', 'title'], {
+      sort: [{ key: 'title', order: 1 }],
+    });
+    expect(sql).toEqual('SELECT id, title FROM book ORDER BY title ASC');
   });
 });
--- libs/db-connectors/sql-converter/src/lib/sql-converter.ts
+++ libs/db-connectors/sql-converter/src/lib/sql-converter.ts
@@ -1,8 +1,27 @@
 import { GaqDbQueryOptions, GaqRootQueryFilter } from '@gaq';
-export function sqlConverter<T extends object>(
-  table: string,
-): (
-  filters: GaqRootQueryFilter<T>,
-  selectedFields: string[],
-  opts: GaqDbQueryOptions,
-):  string => {};
+import { isNullOrUndefinedOrEmptyObject } from 'libs/gaq/src/lib/utils';
+export const sqlConverter =
+  <T extends object>(table: string) =>
+  (
+    filters: GaqRootQueryFilter<T>,
+    selectedFields: string[],
+    opts: Pick<GaqDbQueryOptions, 'limit' | 'offset' | 'sort'>
+  ): string => {
+    let sql = `SELECT ${selectedFields.join(', ')} FROM ${table}`;
+    if (!isNullOrUndefinedOrEmptyObject(filters)) {
+      const whereClause = ``;
+    }
+    if (opts.limit) {
+      sql += ` LIMIT ${opts.limit}`;
+    }
+    if (opts.offset) {
+      sql += ` OFFSET ${opts.offset}`;
+    }
+    if (opts.sort) {
+      sql += ` ORDER BY ${opts.sort
+        .map((sort) => `${sort.key} ${sort.order ? 'ASC' : 'DESC'}`)
+        .join(', ')}`;
+    }
+
+    return sql;
+  };
